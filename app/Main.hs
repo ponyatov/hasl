408857a037be66b3680490970a583763ca5883dc
Dmitry Ponyatov <dponyatov@acer.local>
2024-08-04 13:06:13 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@acer.local>

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,17 @@
 module Main where
 
-import System.Environment (getArgs)
-
 import Lib
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-    putStrLn "Hello, Haskell!"
-    getArgs >>= print
-    Lib.some
+  putStrLn "Hello, Haskell!"
+  getArgs >>= print
+  Lib.some
 
-square :: Num a => a -> a
+square :: (Num a) => a -> a
 square v = v * v
+
 -- square 2
 -- (+) 1 2
 -- p.24
