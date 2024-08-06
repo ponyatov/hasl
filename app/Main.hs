8b127bb79181eb9acbc2e5421934b93c709be509
Dmitry Ponyatov <dponyatov@acer.local>
2024-08-06 14:30:25 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@acer.local>

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,17 @@
 module Main where
 
-import Lib
 import System.Environment (getArgs)
+
+import Lib
+import Parser
+-- import AST
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   getArgs >>= print
   Lib.some
+  Parser.test
 
 square :: (Num a) => a -> a
 square v = v * v
@@ -18,6 +22,7 @@ isquare v = v * v
 prod :: (Num a) => a -> a -> a
 prod x y = x * y
 
--- square 2
--- (+) 1 2
--- p.28
+pi :: Double
+pi = 3.1415
+
+-- p.34
---
+++ app/Parser.hs
@@ -0,0 +1,5 @@
+module Parser where
+
+test :: IO ()
+test = do
+  putStrLn "pass"
