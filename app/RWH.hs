1853917a5f66e1bcd87af262c10785012505acc8
Dmitry Ponyatov <dponyatov@acer.local>
2024-08-11 14:05:36 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@acer.local>

--- app/RWH.hs
+++ app/RWH.hs
@@ -3,3 +3,21 @@
 -- Real World Haskell
 
 main = print "Hello"
+
+-- file: ch00/KMinima.hs
+-- lines beginning with "--" are comments.
+minima k xs = take k (sort xs)
+
+-- rwh.20
+
+let e = exp 1
+:info e
+
+(e ** pi) - pi
+
+-- rwh.25
+:set +t
+'c'
+['l','i','s','t']
+"list"
+
