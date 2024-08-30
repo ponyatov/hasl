bbeb22f60c131b9803da1ec502aefa161318c2b8
Dmitry Ponyatov <dponyatov@gmail.com>
2024-08-30 07:25:29 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/ohaskell.hs
+++ app/ohaskell.hs
@@ -1,1 +1,18 @@
-[[1, 2, 3], [4, 5, 6]]
+let m = [[1, 2, 3], [4, 5, 6]]
+
+head m
+tail m
+
+print m
+
+length m
+
+length "length"
+
+:t [1..9]
+
+:t (1,2.3,"45")
+
+pair a b = (a,b)
+
+length (pair 1 2.3)
